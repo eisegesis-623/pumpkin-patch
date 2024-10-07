f892176d6810e14125084f46791ff72163bff347
rhodioo <intovechi@gmail.com>
2024-10-07 00:57:56 -0400

    init template for matching game

--- PumpkinPatch/Assets/Match3/Scripts/GridObject.cs
+++ PumpkinPatch/Assets/Match3/Scripts/GridObject.cs
@@ -1,17 +1,20 @@
-﻿namespace Match3
-{
-    public partial class GridObject<T>
-    {
-
+﻿namespace Match3 {
+    public class GridObject<T> {
         GridSystem2D<GridObject<T>> grid;
         int x;
         int y;
-
-        public GridObject(GridSystem2D<GridObject<T>> grid, int x, int y)
-        {
+        T gem;
+        
+        public GridObject(GridSystem2D<GridObject<T>> grid, int x, int y) {
             this.grid = grid;
             this.x = x;
             this.y = y;
         }
+
+        public void SetValue(T gem) {
+            this.gem = gem;
+        }
+        
+        public T GetValue() => gem;
     }
 }
--- PumpkinPatch/Assets/Match3/Scripts/GridSystem2D.cs
+++ PumpkinPatch/Assets/Match3/Scripts/GridSystem2D.cs
@@ -1,33 +1,34 @@
-using System;
+﻿using System;
 using TMPro;
 using UnityEngine;
 
-namespace Match3
-{
-    public class GridSystem2D<T>
-    {
-        int width;
-        int height;
-        float cellSize;
-        Vector3 origin;
-        T[,] gridArray;
+namespace Match3 {
+    /// <summary>
+    /// A generic 2D grid implementation that can be used in both 2D and 3D.
+    /// The grid stores values of a specified type and provides methods for accessing and modifying
+    /// these values based on grid coordinates or world coordinates. The grid can be displayed
+    /// either vertically (X-Y plane) or horizontally (X-Z plane) using a CoordinateConverter.
+    /// </summary>
+    public class GridSystem2D<T> {
+        readonly int width;
+        readonly int height;
+        readonly float cellSize;
+        readonly Vector3 origin;
+        readonly T[,] gridArray;
 
-        CoordinateConverter coordinateConverter;
+        readonly CoordinateConverter coordinateConverter;
 
         public event Action<int, int, T> OnValueChangeEvent;
-
-        public static GridSystem2D<T> VerticalGrid(int width, int height, float cellSize, Vector3 origin, bool debug = false)
-        {
+        
+        public static GridSystem2D<T> VerticalGrid(int width, int height, float cellSize, Vector3 origin, bool debug = false) {
             return new GridSystem2D<T>(width, height, cellSize, origin, new VerticalConverter(), debug);
         }
-
-        public static GridSystem2D<T> HorizontalGrid(int width, int height, float cellSize, Vector3 origin, bool debug = false)
-        {
+        
+        public static GridSystem2D<T> HorizontalGrid(int width, int height, float cellSize, Vector3 origin, bool debug = false) {
             return new GridSystem2D<T>(width, height, cellSize, origin, new HorizontalConverter(), debug);
         }
 
-        public GridSystem2D(int width, int height, float cellSize, Vector3 origin, CoordinateConverter coordinateConverter, bool debug)
-        {
+        public GridSystem2D(int width, int height, float cellSize, Vector3 origin, CoordinateConverter coordinateConverter, bool debug) {
             this.width = width;
             this.height = height;
             this.cellSize = cellSize;
@@ -35,53 +36,43 @@ namespace Match3
             this.coordinateConverter = coordinateConverter ?? new VerticalConverter();
 
             gridArray = new T[width, height];
-
-            if (debug)
-            {
+            
+            if (debug) {
                 DrawDebugLines();
             }
         }
-
+        
         // Set a value from a grid position
-
-        public void SetValue(Vector3 worldPosition, T value)
-        {
+        public void SetValue(Vector3 worldPosition, T value) {
             Vector2Int pos = coordinateConverter.WorldToGrid(worldPosition, cellSize, origin);
             SetValue(pos.x, pos.y, value);
         }
 
-        public void SetValue(int x, int y, T value)
-        {
-            if (IsValid(x, y))
-            {
+        public void SetValue(int x, int y, T value) {
+            if (IsValid(x, y)) {
                 gridArray[x, y] = value;
                 OnValueChangeEvent?.Invoke(x, y, value);
             }
         }
-
+        
         // Get a value from a grid position
-
-        public T GetValue(Vector3 worldPosition)
-        {
+        public T GetValue(Vector3 worldPosition) {
             Vector2Int pos = GetXY(worldPosition);
             return GetValue(pos.x, pos.y);
         }
 
-        public T GetValue(int x, int y)
-        {
-            return IsValid(x, y) ? gridArray[x, y] : default(T);
+        public T GetValue(int x, int y) {
+            return IsValid(x, y) ? gridArray[x, y] : default;
         }
 
-        // Is the input coordinates valid?
-
-        bool IsValid(int x, int y) => x >= 0 && x < width && y < height;
-
+        bool IsValid(int x, int y) => x >= 0 && y >= 0 && x < width && y < height;
+        
         public Vector2Int GetXY(Vector3 worldPosition) => coordinateConverter.WorldToGrid(worldPosition, cellSize, origin);
-
+        
         public Vector3 GetWorldPositionCenter(int x, int y) => coordinateConverter.GridToWorldCenter(x, y, cellSize, origin);
 
         Vector3 GetWorldPosition(int x, int y) => coordinateConverter.GridToWorld(x, y, cellSize, origin);
-
+        
         void DrawDebugLines() {
             const float duration = 100f;
             var parent = new GameObject("Debugging");
@@ -118,65 +109,56 @@ namespace Match3
 
         public abstract class CoordinateConverter {
             public abstract Vector3 GridToWorld(int x, int y, float cellSize, Vector3 origin);
-
+            
             public abstract Vector3 GridToWorldCenter(int x, int y, float cellSize, Vector3 origin);
 
             public abstract Vector2Int WorldToGrid(Vector3 worldPosition, float cellSize, Vector3 origin);
 
             public abstract Vector3 Forward { get; }
         }
-
+        
         /// <summary>
-        /// A cordinate converter for vertical grids, where the grid lies on the X-Y plane.
+        /// A coordinate converter for vertical grids, where the grid lies on the X-Y plane.
         /// </summary>
-        public  class VerticalConverter : CoordinateConverter
-        {
-            public override Vector3 GridToWorld(int x, int y, float cellSize, Vector3 origin)
-            {
+        public class VerticalConverter : CoordinateConverter {
+            public override Vector3 GridToWorld(int x, int y, float cellSize, Vector3 origin) {
                 return new Vector3(x, y, 0) * cellSize + origin;
             }
-
-            public override Vector3 GridToWorldCenter(int x, int y, float cellSize, Vector3 origin)
-            {
+            
+            public override Vector3 GridToWorldCenter(int x, int y, float cellSize, Vector3 origin) {
                 return new Vector3(x * cellSize + cellSize * 0.5f, y * cellSize + cellSize * 0.5f, 0) + origin;
             }
 
-            public override Vector2Int WorldToGrid(Vector3 worldPosition, float cellSize, Vector3 origin)
-            {
+            public override Vector2Int WorldToGrid(Vector3 worldPosition, float cellSize, Vector3 origin) {
                 Vector3 gridPosition = (worldPosition - origin) / cellSize;
                 var x = Mathf.FloorToInt(gridPosition.x);
                 var y = Mathf.FloorToInt(gridPosition.y);
                 return new Vector2Int(x, y);
             }
-
+        
             public override Vector3 Forward => Vector3.forward;
         }
-
+        
         /// <summary>
         /// A coordinate converter for horizontal grids, where the grid lies on the X-Z plane.
         /// </summary>
-        public class HorizontalConverter : CoordinateConverter
-        {
-            public override Vector3 GridToWorldCenter(int x, int y, float cellSize, Vector3 origin)
-            {
+        public class HorizontalConverter : CoordinateConverter {
+            public override Vector3 GridToWorldCenter(int x, int y, float cellSize, Vector3 origin) {
                 return new Vector3(x * cellSize + cellSize * 0.5f, 0, y * cellSize + cellSize * 0.5f) + origin;
             }
 
-            public override Vector3 GridToWorld(int x, int y, float cellSize, Vector3 origin)
-            {
+            public override Vector3 GridToWorld(int x, int y, float cellSize, Vector3 origin) {
                 return new Vector3(x, 0, y) * cellSize + origin;
             }
 
-            public override Vector2Int WorldToGrid(Vector3 worldPosition, float cellSize, Vector3 origin)
-            {
+            public override Vector2Int WorldToGrid(Vector3 worldPosition, float cellSize, Vector3 origin) {
                 Vector3 gridPosition = (worldPosition - origin) / cellSize;
                 var x = Mathf.FloorToInt(gridPosition.x);
                 var y = Mathf.FloorToInt(gridPosition.z);
                 return new Vector2Int(x, y);
             }
-
+        
             public override Vector3 Forward => -Vector3.up;
         }
-
     }
 }
