dc89c6b2ecaa8e30e09f432f497aa31db70e2d24
rhodioo <intovechi@gmail.com>
2024-10-06 21:17:13 -0400

    Basic Grid

---
+++ PumpkinPatch/Assets/Match3/Scripts/Gem.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Match3
+{
+    public class Gem : MonoBehaviour
+    {
+
+    }
+}
---
+++ PumpkinPatch/Assets/Match3/Scripts/GridObject.cs
@@ -0,0 +1,17 @@
+﻿namespace Match3
+{
+    public partial class GridObject<T>
+    {
+
+        GridSystem2D<GridObject<T>> grid;
+        int x;
+        int y;
+
+        public GridObject(GridSystem2D<GridObject<T>> grid, int x, int y)
+        {
+            this.grid = grid;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
