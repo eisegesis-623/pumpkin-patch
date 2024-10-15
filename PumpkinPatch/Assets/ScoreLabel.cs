fed767a1d2f3866d91d5887071f10144afece030
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2024-10-15 00:30:14 -0400

    Added score tracking

--- PumpkinPatch/Assets/ScoreLabel.cs
+++ PumpkinPatch/Assets/ScoreLabel.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreLabel : MonoBehaviour
 {
     public Match3.Match3 match3;
+
+    private void Update()
+    {
+        GetComponent<TextMeshProUGUI>().text = (match3.AllMatches.Count / 3).ToString();
+    }
 }
